3aee3fb976c98b245db126ce573bbd47a02fa4f0
hamelln <vusdus91@gmail.com>
2023-08-07 15:56:18 +0900

    fix: 포트 번호 3000으로 변경

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import BreweryService from "./services/breweryService";
 passportConfig();
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
