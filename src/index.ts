efbd3a5872695ac57be5cfbeaa11d2ce4e74dc71
hamelln <vusdus91@gmail.com>
2023-08-07 20:37:30 +0900

    fix: normalizePort 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import app from "./app";
 
-app.listen(3008, () => {
+var port = normalizePort(process.env.PORT || "3008");
+
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${3008}`);
 });
+
+function normalizePort(val: string | number | any) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
