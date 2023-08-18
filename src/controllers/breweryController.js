9abcd15aaad656dd06aabc8a04bfbe23112e340e
hamelln <vusdus91@gmail.com>
2023-08-18 16:58:12 +0900

    fix: 시간 요약 정보 포함한 브루어리 정보를 반환하도록 변경

--- src/controllers/breweryController.js
+++ src/controllers/breweryController.js
@@ -7,7 +7,7 @@ const breweryController = async (req, res) => {
   const breweryId = req.params.id;
   switch (method) {
     case "GET":
-      const brewery = breweryService.getBreweryById(breweryId);
+      const brewery = breweryService.getSummarizedBreweryById(breweryId);
       if (brewery) {
         res.json(brewery);
       } else {
